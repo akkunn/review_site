228b6bea17e3923d2a0f537fa61bf14f48abe003
akkunn <853amtg@gmail.com>
2022-07-02 17:25:24 +0900

    users_controllerの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # get 'users/show'
+  # get 'users/edit'
   root 'searches#home'
   get '/result', to: 'searches#result'
+  resources :users, only: [:show, :edit]
 end
