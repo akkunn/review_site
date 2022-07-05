10b124d5a907e32d7d9c3f8fea506adc493d76ac
akkunn <853amtg@gmail.com>
2022-07-05 15:47:49 +0900

    active_hashの作成

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,2 +1,7 @@
 class School < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+  belongs_to :language
+  belongs_to :cost
+  belongs_to :period
 end
