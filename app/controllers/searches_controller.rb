3c9b6f422274a38632df78022d2fe64ffa95788f
akkunn <853amtg@gmail.com>
2022-07-05 18:06:48 +0900

    検索機能の追加

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,8 +1,33 @@
 class SearchesController < ApplicationController
+
+  before_action :search_school, only: [:home, :result]
+
   def home
-    
+    @schools = School.all
+    set_style_column
+    set_support_column
+    set_guarantee_column
   end
 
   def result
+    @schools = @q.result
+  end
+
+  private
+  def search_school
+    @q = School.ransack(params[:q])
+  end
+
+  # 重複なくカラムのデータを取り出す
+  def set_style_column
+    @school_style = School.select("style").distinct
+  end
+
+  def set_support_column
+    @school_support = School.select("support").distinct
+  end
+
+  def set_guarantee_column
+    @school_guarantee = School.select("guarantee").distinct
   end
 end
