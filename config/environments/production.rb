b6a5bc4bf315bd312eceacb14e5043722315dec3
akkunn <853amtg@gmail.com>
2022-07-06 17:37:59 +0900

    sendgridの設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,17 +64,17 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'myreviewsite.herokuapp.com', protocol: 'https' }
+  config.action_mailer.default_url_options = { host: 'https://myreviewsite.herokuapp.com'}
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
     :address => "smtp.gmail.com",
-    :domain => "gmail.com",
     :port => 587,
-    :user_name => ENV['SEND_MAIL'],
-    :password => ENV['GMAIL_PRODUCTION_PASSWORD'],
+    :domain => "heroku.com",
     :authentication => :plain,
-    :enable_starttls_auto => true
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD']
   }
 
 
