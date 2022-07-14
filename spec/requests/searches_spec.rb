41371a5271b54f3aaf9bc019e577b23f9848c06c
akkunn <853amtg@gmail.com>
2022-07-14 19:01:06 +0900

    スクール名が無記入で検索された場合全てのスクールが検索されることのテストを追加

--- spec/requests/searches_spec.rb
+++ spec/requests/searches_spec.rb
@@ -16,24 +16,53 @@ RSpec.describe "Searches", type: :request do
       expect(response).to have_http_status(200)
     end
 
-    context 'correct parameter' do
-      it "exists correct school" do
-        @params = {}
-        @params[:q] = { name_cont: school.name }
-        @q = School.ransack(@params)
-        @schools = @q.result
-        expect(@schools).to include(school)
-      end
+    it "matches correct school when searching correct name" do
+      @params = {}
+      @params[:q] = { name_cont: school.name }
+      @q = School.ransack(@params)
+      @schools = @q.result
+      expect(@schools).to include(school)
     end
 
-    context 'uncorrect parameter' do
-      it "doesn't exist correct school" do
-        @params = {}
-        @params[:q] = { name_cont: other_school.name }
-        @q = School.ransack(@params)
-        @schools = @q.result
-        expect(@schools.reload).not_to include(school)
-      end
+    it "doesn't match correct school when searching uncorrect name" do
+      @params = {}
+      @params[:q] = { name_cont: other_school.name }
+      @q = School.ransack(@params)
+      @schools = @q.result
+      expect(@schools.reload).not_to include(school)
     end
+
+    it "matches all schools when name is blank" do
+      @params = {}
+      @params[:q] = { name_cont: nil }
+      @q = School.ransack(@params)
+      @schools = @q.result
+      expect(@schools).to include(school)
+      expect(@schools).to include(other_school)
+    end
+
+
+
+
+
+    # context 'correct parameter' do
+    #   it "exists correct school" do
+    #     @params = {}
+    #     @params[:q] = { name_cont: school.name }
+    #     @q = School.ransack(@params)
+    #     @schools = @q.result
+    #     expect(@schools).to include(school)
+    #   end
+    # end
+
+    # context 'uncorrect parameter' do
+    #   it "doesn't exist correct school" do
+    #     @params = {}
+    #     @params[:q] = { name_cont: other_school.name }
+    #     @q = School.ransack(@params)
+    #     @schools = @q.result
+    #     expect(@schools.reload).not_to include(school)
+    #   end
+    # end
   end
 end
