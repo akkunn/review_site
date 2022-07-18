44c1214bc4ad0afe82eae8c285df5bc37c466a7d
akkunn <853amtg@gmail.com>
2022-07-19 08:58:25 +0900

    review.rbのバリデーションテストを追加

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :school
   belongs_to :user
+
+  validates :name, presence: true
+  validates :user_id, presence: true
+  validates :school_id, presence: true
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validation" do
+    let(:school) { FactoryBot.create(:school) }
+    let(:user) { FactoryBot.create(:user) }
+    let(:review) { FactoryBot.build(:review, user_id: user.id, school_id: school.id) }
+
+    it "is valid with a name" do
+      expect(review).to be_valid
+    end
+
+    it "is invalid without a name" do
+      review = FactoryBot.build(:review, name: nil)
+      review.valid?
+      expect(review.errors[:name]).to include("can't be blank")
+    end
+
+    it "is invalid without user_id" do
+      review = FactoryBot.build(:review, user_id: nil)
+      review.valid?
+      expect(review.errors[:user_id]).to include("can't be blank")
+    end
+
+    it "is invalid without school_id" do
+      review = FactoryBot.build(:review, school_id: nil)
+      review.valid?
+      expect(review.errors[:school_id]).to include("can't be blank")
+    end
+  end
 end
