68445f5bdfb29d0145b5ec26053f3d44b33f26d5
akkunn <853amtg@gmail.com>
2022-07-19 11:06:14 +0900

    星評価平均値を求めるメソッドを追加

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,14 @@
 module ReviewsHelper
+  def ave_star(review)
+    review_star_ary = [review.curriculum_star, review.support_star, review.teacher_star, review.compatibility_star]
+    new_review_star_ary = review_star_ary.compact
+    if new_review_star_ary.size != 0
+      sum_star = 0
+      new_review_star_ary.each do |star|
+        sum_star += star
+      end
+      ave_star = sum_star / new_review_star_ary.size
+      ((ave_star * 2.0).round / 2.0).to_f
+    end
+  end
 end
