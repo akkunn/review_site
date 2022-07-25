58ac98450ccbd78bd1bfeaea86fdc5fd7aace64b
akkunn <853amtg@gmail.com>
2022-07-25 18:05:14 +0900

    タイムゾーンを日本時間に変更し、2022/01/01 12:34のような形に修正

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,6 @@ module ReviewSite
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = 'Tokyo'
   end
 end
