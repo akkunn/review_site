0550a154357de206bb6a1f59b612076531fd43ae
akkunn <853amtg@gmail.com>
2022-08-13 11:45:47 +0900

    口コミ新規投稿・編集画面のテキストエリアの高さを自動で調節する機能を追加

--- app/javascript/packs/resize.js
+++ app/javascript/packs/resize.js
@@ -6,4 +6,12 @@ window.addEventListener("turbolinks:load", function(){
       textarea.style.cssText = 'height : ' + textarea.scrollHeight + 'px;';
     },0);
   });
+
+  $('textarea.resize7').on('keydown', function(){
+    var textarea = this;
+    setTimeout(function(){
+      textarea.style.cssText = 'height : 7.1rem;';
+      textarea.style.cssText = 'height : ' + textarea.scrollHeight + 'px;';
+    },0);
+  });
 });
