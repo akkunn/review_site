ca7b5b5dede39bb10db25b8ff1bb877e76359e87
akkunn <853amtg@gmail.com>
2022-08-16 15:17:41 +0900

    ホーム画面のクラスを変更したため、スクール検索のテストのwithinを修正

--- spec/system/searches_spec.rb
+++ spec/system/searches_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Searches", type: :system do
-  let(:school) { FactoryBot.create(:school) }
+  let!(:school) { FactoryBot.create(:school) }
 
   before do
     driven_by(:rack_test)
@@ -9,9 +9,15 @@ RSpec.describe "Searches", type: :system do
 
   scenario "user searches schools" do
     visit root_path
-    within '.box' do
-      fill_in "スクール名", with: school.name
-      select school.language.name, from: "学習言語"
+    within '.school-search-box' do
+      fill_in "q_name_cont", with: school.name
+      select school.language.name, from: "q_language_id_eq"
+      select school.cost.range, from: "q_cost_id_eq"
+      select school.period.range, from: "q_period_id_eq"
+      select school.style, from: "q_style_eq"
+      select school.prefecture.name, from: "q_prefecture_id_eq"
+      select school.support, from: "q_support_eq"
+      select school.guarantee, from: "q_guarantee_eq"
       click_button "検索"
     end
     expect(page).to have_content(school.name)
