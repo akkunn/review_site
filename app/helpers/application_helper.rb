27b8186053d8c180989c0fbb4b91a48b4f2e1cb8
akkunn <853amtg@gmail.com>
2022-08-20 20:54:52 +0900

    ページごとにページタイトルを表示する機能とそのテストを追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  BASE_TITLE = "プロコミ | プログラミングスクール口コミ・質問サイト".freeze
+
+  def full_title(page_title)
+    page_title.blank? ? BASE_TITLE : "#{page_title} | #{BASE_TITLE}"
+  end
 end
