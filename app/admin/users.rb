eb900b4ee6a55ad18f8e50997a32f5e73d00fb6d
akkunn <853amtg@gmail.com>
2022-08-23 16:06:58 +0900

    管理画面のユーザーのストロングパラメータを修正

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,12 +1,11 @@
 ActiveAdmin.register User do
 
-  permit_params :email, :password, :password_confirmation, :introduction, :name
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :name, :introduction, :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :confirmation_token, :confirmed_at, :confirmation_sent_at, :unconfirmed_email, :admin
+  permit_params :name, :introduction, :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :confirmation_token, :confirmed_at, :confirmation_sent_at, :unconfirmed_email, :admin
   #
   # or
   #
