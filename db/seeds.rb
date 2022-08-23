cf3821f8ad1a67abd0021b8f5493131381b68274
akkunn <853amtg@gmail.com>
2022-08-23 13:10:10 +0900

    本番環境でも管理者が作成されるように修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -77,4 +77,4 @@ end
     period_id: 4
   )
 end
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
