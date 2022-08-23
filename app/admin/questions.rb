287ff52fefd6a993f6fbef707a1253f609249ca3
akkunn <853amtg@gmail.com>
2022-08-23 15:32:42 +0900

    管理画面に質問を追加

---
+++ app/admin/questions.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Question do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  permit_params :name, :content, :user_id, :school_id, :solution
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:name, :content, :user_id, :school_id, :solution]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+
+end
