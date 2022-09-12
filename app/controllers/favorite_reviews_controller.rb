3cf0a61005b59e61b31648658cff1a335cf7305b
akkunn <853amtg@gmail.com>
2022-09-12 19:44:09 +0900

    いいね解除機能を追加

--- app/controllers/favorite_reviews_controller.rb
+++ app/controllers/favorite_reviews_controller.rb
@@ -1,6 +1,11 @@
 class FavoriteReviewsController < ApplicationController
   def create
-    Favorite_review.create(user_id: current_user.id, review_id: params[:id])
+    FavoriteReview.create(user_id: current_user.id, review_id: params[:id])
+    redirect_to root_path
+  end
+
+  def destroy
+    FavoriteReview.find_by(user_id: current_user.id, review_id: params[:id]).destroy
     redirect_to root_path
   end
 end
