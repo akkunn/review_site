06c8e3bf62fe87d280b813ea4e7906e34ad19fc9
akkunn <853amtg@gmail.com>
2022-09-12 23:41:59 +0900

    非同期いいね機能を追加

--- app/controllers/favorite_reviews_controller.rb
+++ app/controllers/favorite_reviews_controller.rb
@@ -1,11 +1,17 @@
 class FavoriteReviewsController < ApplicationController
+  before_action :review_params
+
   def create
     FavoriteReview.create(user_id: current_user.id, review_id: params[:id])
-    redirect_to root_path
   end
 
   def destroy
     FavoriteReview.find_by(user_id: current_user.id, review_id: params[:id]).destroy
-    redirect_to root_path
+  end
+
+  private
+
+  def review_params
+    @review = Review.find(params[:id])
   end
 end
