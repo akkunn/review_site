68e9c87b7dd54a4455ff3cf022158df8063a1511
akkunn <853amtg@gmail.com>
2022-09-13 17:46:44 +0900

    口コミ詳細画面のいいね機能のシステムスペックを追加

--- spec/system/favorite_reviews_spec.rb
+++ spec/system/favorite_reviews_spec.rb
@@ -5,38 +5,64 @@ RSpec.describe "FavoriteReviews", type: :system do
   let!(:school) { FactoryBot.create(:school) }
   let!(:review) { FactoryBot.create(:review, user_id: user.id, school_id: school.id) }
 
-  scenario "user likes a review", js: true do
-    visit root_path
-    find('#menu-button').click
-    click_link "ログイン"
-    fill_in "メールアドレス", with: user.email
-    fill_in "パスワード", with: user.password
-    click_button "ログイン"
-    expect(page).to have_content "ログインしました。"
-    click_button "検索"
-    click_link school.name
-    expect {
-      click_link nil, href: create_favorite_review_path(review)
-      expect(page).to have_content(1)
-      expect(review.reload.favorite_reviews.count).to eq(1)
-    }.to change(review.favorite_reviews, :count).by(1)
+  describe "favorite_review", js: true do
+    before do
+      visit root_path
+      find('#menu-button').click
+      click_link "ログイン"
+      fill_in "メールアドレス", with: user.email
+      fill_in "パスワード", with: user.password
+      click_button "ログイン"
+      click_button "検索"
+      click_link school.name
+    end
+
+    scenario "user likes a review in school-show-page" do
+      expect {
+        click_link nil, href: create_favorite_review_path(review)
+        expect(page).to have_content(1)
+        expect(review.reload.favorite_reviews.count).to eq(1)
+      }.to change(review.favorite_reviews, :count).by(1)
+    end
+
+    scenario "user likes a review in review-show-page" do
+      click_link review.name
+      expect {
+        click_link nil, href: create_favorite_review_path(review)
+        expect(page).to have_content(1)
+        expect(review.reload.favorite_reviews.count).to eq(1)
+      }.to change(review.favorite_reviews, :count).by(1)
+    end
   end
 
-  scenario "user deletes a favorite_review", js: true do
-    FactoryBot.create(:favorite_review, user_id: user.id, review_id: review.id)
-    visit root_path
-    find('#menu-button').click
-    click_link "ログイン"
-    fill_in "メールアドレス", with: user.email
-    fill_in "パスワード", with: user.password
-    click_button "ログイン"
-    expect(page).to have_content "ログインしました。"
-    click_button "検索"
-    click_link school.name
-    expect {
-      click_link nil, href: destroy_favorite_review_path(review)
-      expect(page).to have_content(0)
-      expect(review.reload.favorite_reviews.count).to eq(0)
-    }.to change(review.favorite_reviews, :count).by(-1)
+  describe "out_favorite_review", js: true do
+    before do
+      FactoryBot.create(:favorite_review, user_id: user.id, review_id: review.id)
+      visit root_path
+      find('#menu-button').click
+      click_link "ログイン"
+      fill_in "メールアドレス", with: user.email
+      fill_in "パスワード", with: user.password
+      click_button "ログイン"
+      click_button "検索"
+      click_link school.name
+    end
+
+    scenario "user deletes a favorite_review in school-show-page" do
+      expect {
+        click_link nil, href: destroy_favorite_review_path(review)
+        expect(page).to have_content(0)
+        expect(review.reload.favorite_reviews.count).to eq(0)
+      }.to change(review.favorite_reviews, :count).by(-1)
+    end
+
+    scenario "user deletes a favorite_review in review-show-page" do
+      click_link review.name
+      expect {
+        click_link nil, href: destroy_favorite_review_path(review)
+        expect(page).to have_content(0)
+        expect(review.reload.favorite_reviews.count).to eq(0)
+      }.to change(review.favorite_reviews, :count).by(-1)
+    end
   end
 end
