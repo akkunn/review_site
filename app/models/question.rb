74877104c86a28590841ab999cc4b8e4ab061b7d
akkunn <853amtg@gmail.com>
2022-09-16 23:35:57 +0900

    questionモデルとnotificationモデルのリレーションを追加

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,7 @@ class Question < ApplicationRecord
   belongs_to :user
   belongs_to :school
   has_many :answers, dependent: :destroy
+  has_many :notifications, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 60 }
   validates :content, presence: true
