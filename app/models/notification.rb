1570487c370292d58642af9893d2784a9ad63251
akkunn <853amtg@gmail.com>
2022-09-16 21:57:53 +0900

    通知一覧画面を追加

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -4,4 +4,12 @@ class Notification < ApplicationRecord
   belongs_to :comment, optional: true
   belongs_to :visiter, class_name: 'User', foreign_key: 'visiter_id', optional: true
   belongs_to :visited, class_name: 'User', foreign_key: 'visited_id', optional: true
+
+  def how_long_ago
+    if (Time.now.to_s(:date_jp).to_i - self.created_at.to_s(:date_jp).to_i) == 0
+      '今日'
+    else
+      "#{(Time.now.to_s(:date_jp).to_i - self.created_at.to_s(:date_jp).to_i)}日前"
+    end
+  end
 end
