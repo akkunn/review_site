7661191aa67b2650d31fcf2ed40c642b81a50e1c
akkunn <853amtg@gmail.com>
2022-09-17 22:54:57 +0900

    rubocopの指摘により不要なコードや非推奨なコードを修正

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,7 +2,8 @@ class NotificationsController < ApplicationController
   before_action :authenticate_user!, only: [:index, :destroy, :destroy_all]
 
   def index
-    @notifications = current_user.passive_notifications.paginate(page: params[:page]).per(30)
+    @notifications = current_user.passive_notifications.
+      paginate(page: params[:page]).per(30).order_new
     @notifications.where(checked: false).each do |notification|
       notification.update(checked: true)
     end
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,16 +1,16 @@
 class Notification < ApplicationRecord
-  default_scope -> { order(created_at: :desc) }
+  scope :order_new, -> { order(created_at: :desc) }
   belongs_to :review, optional: true
   belongs_to :answer, optional: true
   belongs_to :question, optional: true
-  belongs_to :visiter, class_name: 'User', foreign_key: 'visiter_id', optional: true
-  belongs_to :visited, class_name: 'User', foreign_key: 'visited_id', optional: true
+  belongs_to :visiter, class_name: 'User', optional: true
+  belongs_to :visited, class_name: 'User', optional: true
 
   def how_long_ago
-    if (Time.now.to_s(:date_jp).to_i - self.created_at.to_s(:date_jp).to_i) == 0
+    if (Time.now.to_s(:date_jp).to_i - created_at.to_s(:date_jp).to_i) == 0
       '今日'
     else
-      "#{(Time.now.to_s(:date_jp).to_i - self.created_at.to_s(:date_jp).to_i)}日前"
+      "#{(Time.now.to_s(:date_jp).to_i - created_at.to_s(:date_jp).to_i)}日前"
     end
   end
 end
